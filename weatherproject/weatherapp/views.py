a7703d8fa38efccac1e80257fd322038b590af58
Abu <abuabrar520724@gmail.com>
2025-06-30 00:32:19 +0530

    weather app done

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -4,13 +4,21 @@ import datetime
 # Create your views here.
 def home(request):
     if 'city' in request .POST:
-        city = request.POST['city']
+
+        city = request.POST.get('city')
     else:
         city = 'indore'
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=1787ac1358a851089898c332b5ce1837'
     PARAMS = {'units': 'metric'}
 
-    data = request.get(url,PARAMS).json()
+    data = requests.get(url, params = PARAMS).json()
+
+    if data.get('cod') != 200:
+        return render(request, 'weatherapp/index.html', {
+            'error': 'City not found. Please enter a valid city name.',
+            'day': datetime.date.today(),
+        })
+
 
     description = data['weather'][0]['description']
     icon = data['weather'] [0] ['icon']
@@ -18,4 +26,4 @@ def home(request):
 
 
     day = datetime.date.today()
-    return render(request,'weatherapp/index.html',{'description':description,'icon':icon,'temp':temp,'day':day})
+    return render(request,'weatherapp/index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city})
